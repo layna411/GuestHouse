36ac2a6a1a4368469cc3a6848a983e23b3843bbd
layna411 <layna4115@gmail.com>
2026-06-08 09:16:51 +0530

    update

--- backend/config.py
+++ backend/config.py
@@ -21,6 +21,11 @@ class Config:
     DB_USER = os.getenv("DB_USER", "root")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "")
     DB_HOST = os.getenv("DB_HOST", "localhost")
+
+
+
+
+    
     DB_PORT = os.getenv("DB_PORT", "3306")
     DB_NAME = os.getenv("DB_NAME", "GuestHouse")
 
